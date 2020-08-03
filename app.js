6865daf142ca3f0588098710cf3379b5a6d137e4
miaaaz <zhao.mia@northeastern.edu>
2020-08-02 21:46:29 -0700

    Add database

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
+const keys = require("./config/keys");
 
 const app = express();
-
-const items = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
+// Database initialization
+const itemSchema = {
+    name : String
+};
+
+// Mongoose model usually is capitalized
+const Item = mongoose.model('Item', itemSchema);
+
+
+// Homepage
 app.get('/', (req, res) => {
-    let day = date();
-    res.render('list', {kindOfDay: day, newListItems: items});
+    const day = date();
+    Item.find({}, (err, foundItems) => {
+        res.render('list', {kindOfDay: day, newListItems: foundItems});
+    });
+    
 });
 
+// Add new item
 app.post('/', (req, res) => {
-    let item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
-})
+    const itemName = req.body.newItem;
+    Item.create({ name: itemName }, (err, newItem) => {
+        if (!err) {
+            console.log(`successfully save ${newItem.name}`);
+            res.redirect('/');
+        }
+    });
+});
+
+// Delete item
+app.post('/delete', (req, res) => {
+    const deleteItemID = req.body.checkbox;
+    Item.findByIdAndRemove(deleteItemID, (err) => {
+        if (!err) {
+            console.log('successfully delete');
+            res.redirect('/');
+        }
+    })
+});
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 app.listen(3000, () => {
     console.log("Start listening on port 3000");
